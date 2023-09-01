d9ae9a997c69dc2d2098ac07a564988cefd37c14
Yukti Agarwal <yukti@finmapp.com>
2023-09-01 15:52:56 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class WebSocketScreen extends StatefulWidget {
 
 class _WebSocketScreenState extends State<WebSocketScreen> {
   late WebSocketChannel? channel;
+  final Map<String, dynamic>? headers = {};
 
   @override
   void initState() {
@@ -35,14 +36,7 @@ class _WebSocketScreenState extends State<WebSocketScreen> {
 
   Future<void> _connectWebSocket() async {
     const socketUrl = 'ws://smartapisocket.angelone.in/smart-stream';
-    channel = IOWebSocketChannel.connect(socketUrl, headers: {
-      'Authorization':
-          'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImU4YTIzZjM5LWVjM2YtNDczNi1hMjExLTY0N2E3MTdiNjMzNSJ9.eyJtb2JpbGVOdW1iZXIiOiI5OTEwMTc4NDk9nikkiwidXNlclR5cGUiOiJDVVNUT01FUiIsInVzZXJJZCI6ImRiMGFmZGIwLTJmNmYtMTFlZS1hYmE9ikI5ZGQ5MTA2ZGJjNTQiLCJsb2dpblR5cGUiOiJNT0JJTEVfTlVNQkVSX09UUCIsInJvbGUiOiJjdXN0b21lciIsImRldmljZUlkIjoiZWRlYjQ1OTAtN2I4MS0xMWVkLWIyOWYtZGJhODdlYTEwYmZmIiwicGxhdGZvcm0iOiJBTiIsImtleWlkIjoiZThhMjNmMzktZWMzZi00NzM2LWEyMTEtNjQ3YTcxN2I2MzM1IiwianRpIjoiVGJwWno5ZTVjSCIsImlhdCI6MTY5MzU1NTMxMSwiZXhwIjoxNjkzOTc1MzExLCJhdWQiOlsiQ1VTVE9NRVIiXSwiaXNzIjoiZmluaW5maW5pdHkgdGVjaG5vbG9neSBwcml2YXRlIGxpbWl0ZWQiLCJzdWIiOiJkZXZlbG9wZXJAZWF6eWZpbi5jb20ifQ.MIP4V9nmiXHqIy1C0gia8NP5BaV59HOXQ5Zd_Ioh5kL--MmKkoheHxxfafgJYCgcAYYFILDugkDmKCw2tt-PtqW1SaiP6SOLrFrGYB2gQkkOh4F2vIhHED0fdCw3SxmQiWMivNuTI9cewitC_Jwox60qMpq73dJOEQUGEBBvHHY',
-      'x-api-key': 'Nvfk4nJJ',
-      'x-client-code': "h137892",
-      'x-feed-token':
-          "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IkgxMzc4OTIiLCJpYXQiOjE2OTM1NTUzOTcsImV4cCI6MTY5MzY0MTc5N30.bot3p41Z-cIktYvPwbeQDssD-W4X5Q7jVjDu1lvkklirX59_9F1xCdRHPov_SLhGRjS1uSlZtUvmagHADzqnkg",
-    });
+    channel = IOWebSocketChannel.connect(socketUrl, headers: headers);
 
     // Listen for incoming WebSocket data.
     channel?.stream.listen((data) {
